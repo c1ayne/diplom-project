37b85e130c0c991aabc25fbd2631eaac6907047a
clayne <savva665544332211@yandex.ru>
2026-01-02 05:38:59 +0300

    create data generating

--- generator/src/main/java/ru/diplom/App.java
+++ generator/src/main/java/ru/diplom/App.java
@@ -1,13 +1,66 @@
 package ru.diplom;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+public class App {
+
+    private static final String TOPIC_SENSOR_TEMPLATE = "sensors/factory/%s/telemetry";
+    private static final String TOPIC_ALERT_TEMPLATE = "alerts/factory/%s/alarm";
+
+    public static void main( String[] args ) {
+        try {
+            MqttClient client = MqttManager.createClient();
+
+            ObjectMapper mapper = new ObjectMapper();
+            Random random = new Random();
+
+            String[] deviceIds = {"sensor-01", "sensor-02", "sensor-03", "pump-A", "pump-B"};
+
+            System.out.println("Starting generation loops");
+
+            while (true) {
+                String deviceId = deviceIds[random.nextInt(deviceIds.length)];
+
+                SensorData data = new SensorData();
+                data.setDeviceId(deviceId);
+                data.setTimestamp(System.currentTimeMillis());
+
+                boolean isCritical = random.nextInt(10) == 0;
+
+                String topic;
+
+                if (isCritical) {
+                    data.setType("CRITICAL_OVERHEAT");
+                    data.setValue(80.0 + random.nextDouble() * 20);
+                    topic = String.format(TOPIC_ALERT_TEMPLATE, deviceId);
+                } else {
+                    data.setType("TEMPERATURE");
+                    data.setValue(20.0 + random.nextDouble() * 10);
+                    topic = String.format(TOPIC_SENSOR_TEMPLATE, deviceId);
+                }
+
+                String jsonPayload = mapper.writeValueAsString(data);
+
+                MqttMessage message = new MqttMessage(jsonPayload.getBytes());
+                message.setQos(1);
+
+                client.publish(topic, message);
+                System.out.println("Sent to [" + topic + "]: " + jsonPayload);
+
+                TimeUnit.MILLISECONDS.sleep(500);
+            }
+        } catch (MqttException e) {
+            System.err.println("MQTT Error: " + e.getMessage());
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
--- generator/src/main/java/ru/diplom/SensorData.java
+++ generator/src/main/java/ru/diplom/SensorData.java
@@ -2,11 +2,13 @@ package ru.diplom;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class SensorData {
     String deviceId;
     Long timestamp;
