59b39ceae7716bc2fdecaa35c81f5e9592cbd68d
clayne <savva665544332211@yandex.ru>
2026-01-02 03:51:57 +0300

    create generator project

---
+++ generator/src/main/java/ru/diplom/App.java
@@ -0,0 +1,13 @@
+package ru.diplom;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
