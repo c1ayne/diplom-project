4acbac6a46ea6dfe6efb565d4a77fb71a4494fea
clayne <savva665544332211@yandex.ru>
2026-01-09 10:37:25 +0300

    create KafkaConfig (DLT)

---
+++ consumer-service/src/main/java/ru/diplom/consumerservice/config/KafkaConfig.java
@@ -0,0 +1,26 @@
+package ru.diplom.consumerservice.config;
+
+import org.apache.kafka.common.TopicPartition;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaOperations;
+import org.springframework.kafka.listener.CommonErrorHandler;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.FixedBackOff;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public CommonErrorHandler errorHandler(KafkaOperations<Object, Object> template) {
+        DeadLetterPublishingRecoverer recoverer = new DeadLetterPublishingRecoverer(
+                template,
+                (record, ex) -> {
+                    System.out.println("Sending to DLQ: " + record.topic() + ".DLT | Reason: " + ex.getMessage());
+                    return new TopicPartition(record.topic() + ".DLT", record.partition());
+                });
+
+        return new DefaultErrorHandler(recoverer, new FixedBackOff(1000L, 2));
+    }
+}
